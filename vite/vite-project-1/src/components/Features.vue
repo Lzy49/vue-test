0fdc8e27d0d5647f4f1d725bb003b21a7b05f442
lzy <362018513@qq.com>
2021-08-10 16:55:27 +0800

    vite 初体验

---
+++ vite/vite-project-1/src/components/Features.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="featuresBox">
+    <p :class="classNames">你好</p>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss">
+.featuresBox {
+  width: 100px;
+  height: 100px;
+  background: red;
+  > p {
+    color: #fff;
+  }
+}
+</style>
---
+++ vite/vite-project-1/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation {{ stateName }}
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="state.count++">
+    count is: {{ state.count }}
+  </button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { defineProps, reactive, ref } from 'vue';
+console.log('axios', axios);
+
+defineProps({
+  msg: String
+});
+const state = reactive({ count: 0 });
+const stateName = ref('张三');
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+div {
+  background: url('../assets/logo.png');
+}
+</style>
