692792596253bbd25fc85ce5965b29b061143a99
lzy <362018513@qq.com>
2021-08-22 19:07:23 +0800

    router test

--- vue-router/vue-router-1/src/router-api-test/index.js
+++ vue-router/vue-router-1/src/router-api-test/index.js
@@ -4,10 +4,13 @@ import {
   createWebHistory,
   createMemoryHistory,
   NavigationFailureType,
-  START_LOCATION
+  START_LOCATION,
+  onBeforeRouteLeave,
+  onBeforeRouteUpdate,
+  
 } from 'vue-router';
 import routes from './routes';
-console.log(NavigationFailureType,START_LOCATION)
+console.log(NavigationFailureType, START_LOCATION);
 const router = createRouter({
   history: createWebHashHistory(''),
   // history:createWebHistory(),
@@ -17,4 +20,23 @@ const router = createRouter({
 //   console.log(to, from);
 //   return true;
 // });
+router.beforeEach((to, form, next) => {
+  console.log('--------------- beforeEach -------------');
+  console.log(to, form);
+  console.log('----------------------------------------');
+  console.log('beforeEach');
+  next();
+});
+router.beforeResolve((to, form) => {
+  console.log(to, form);
+  console.log('%c beforeResolve', 'color:red');
+});
+// router.afterEach((e) => {
+//   console.log(e)
+//   console.log('afterEach');
+//   console.error('报错啦！');
+// });
+router.onError((err) => {
+  console.log(err);
+});
 export default router;
