e268f7bed55dfe7a3851808ca834007f8cc0b722
lzy <362018513@qq.com>
2021-08-25 18:30:22 +0800

    vuex 进阶部分学习

--- vuex-4/src/store/index.js
+++ vuex-4/src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore } from 'vuex';
 import test from './module/test';
+import pluginTest from './plugins/test';
 const store = createStore({
+  strict: import.meta.env.DEV,
   state() {
     return {
       name: 'vuex'
@@ -13,6 +15,9 @@ const store = createStore({
     },
     log() {
       console.log('store log');
+    },
+    set(state, { key, value }) {
+      state[key] = value;
     }
   },
   actions: {
@@ -24,6 +29,10 @@ const store = createStore({
           resolve(true);
         }, 2000);
       });
+    },
+    test({ dispatch }) {
+      console.log('test');
+      return dispatch('setName');
     }
   },
   getters: {
@@ -34,15 +43,31 @@ const store = createStore({
       console.log(getters.getName);
       return (tag) => state.name + tag;
     }
-  }
+  },
   // modules: {
   //   test
-  // }
+  // },
+  plugins: [pluginTest]
 });
 if (!store.hasModule('test')) {
   store.registerModule('test', test);
 }
+store.subscribeAction(
+  (aciton, state) => {
+    console.log(state);
+    console.log('%c subscribeAction', 'color:green');
+  },
+  { prepend: false }
+);
+// store.subscribeAction({
+//   before: (action, state) => {
+//     console.log(`before action ${action.type}`);
+//   },
+//   after: (action, state) => {
+//     console.log(`after action ${action.type}`);
+//   }
+// });
 setTimeout(() => {
-  store.unregisterModule('test');
+  // store.unregisterModule('test');
 }, 1000);
 export default store;
